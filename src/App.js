f1173244ecea0fccf8532ba1fb580a7d1d3b2447
Tran Hai Trieu <haitrieu2524@gmail.com>
2022-12-14 15:20:05 +0700

    #4 Render "Dữ Liệu của Biến" với React Hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>React Hooks</h1>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    let [number, setNumber] = useState(0);
+
+    let handleIncrease = () => {
+        setNumber(number++);
+    };
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1>React Hooks {number}</h1>
+
+                <button onClick={() => handleIncrease()}>Increase</button>
+            </header>
+        </div>
+    );
+};
 
 export default App;
