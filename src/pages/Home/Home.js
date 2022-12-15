69bb1fc107a9e691483c0f4afccafc0822962159
Tran Hai Trieu <haitrieu2524@gmail.com>
2022-12-15 15:23:58 +0700

    #7 Events Qua Ví Dụ Sự Kiện onClick Với React Hook

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,14 @@
 const Home = () => {
+    const handleClick = () => {
+        console.log('Click me');
+    };
+
     return (
         <>
             <h1>Home Page</h1>
+            <button type="button" onClick={handleClick}>
+                Click me !
+            </button>
         </>
     );
 };
